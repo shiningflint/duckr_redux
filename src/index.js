c499f16cc7e57d5008e4530e28d98e1519659af1
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-12-21 14:26:01 +0900

    hooking up our redux app to the google chrome redux extension

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import users from 'redux/modules/users'
 
-const store = createStore(users, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(users, composeEnhancers(
+  applyMiddleware(thunk),
+))
 
 ReactDOM.render(
   <Provider store={store}>
